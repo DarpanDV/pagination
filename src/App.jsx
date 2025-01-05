ea45326e6f22a2410d4ffbd545839265d0f43656
DarpanDV <varshdarpan@gmail.com>
2025-01-05 13:34:51 +0530

    Added visiblity of the left & right arrow

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,27 @@ function App() {
 
   const [product, setProduct] = useState([])
   const [page, setPage] = useState([]);
-  const[pagePointer,setPagePointer]=useState(1);
+  const [pagePointer, setPagePointer] = useState(1);
+  const[leftPointer,setLeftPointer]=useState(false);
+  const[rightPointer,setRightPointer]=useState(true);
+
   useEffect(() => {
     fetchApi()
   }, [])
 
-  useEffect(()=>{
-    if(product.length!=0)
-    {
+  useEffect(() => {
+    if (product.length != 0) {
       setPage(Array.from({ length: product.length / 10 }, (_, id) => id + 1));
     }
-  },[product])
+  }, [product])
+
+  useEffect(()=>{
+    ((pagePointer!=1)?
+      setLeftPointer(true):setLeftPointer(false));
+      
+      ((pagePointer!=product.length/10)?
+      setRightPointer(true):setRightPointer(false));
+  },[pagePointer,product.length])
 
   const fetchApi = async () => {
     try {
@@ -32,18 +42,20 @@ function App() {
 
   }
 
-  const handlePointer=(pg)=>{
+  const handlePointer = (pg) => {
     setPagePointer(pg);
   }
 
-  const handleLeftPointer=()=>{
-    if(pagePointer!=1)
-    setPagePointer(pagePointer=>pagePointer-1)
+  const handleLeftPointer = () => {
+
+    if (pagePointer != 1)
+      setPagePointer(pagePointer => pagePointer - 1)
   }
 
-  const handleRightPointer=()=>{
-    if(pagePointer!=product.length)
-    setPagePointer(pagePointer=>pagePointer+1)
+  const handleRightPointer = () => {
+
+    if (pagePointer != product.length / 10)
+      setPagePointer(pagePointer => pagePointer + 1)
   }
 
   return (
@@ -51,26 +63,25 @@ function App() {
       {product.length != 0 ?
         (<>
           <div className="container">
-            {product.slice((pagePointer*10)-10,pagePointer*(product.length/10)).map((item) => {
+            {product.slice((pagePointer * 10) - 10, pagePointer * (product.length / 10)).map((item) => {
               return (
                 <div key={item.id}>
-                 
                   <img src={item.thumbnail} alt={item.title} />
                   <p>{item.title}</p>
                 </div>)
             })
             }
           </div>
-          <div style={{ display: "flex",justifyContent:"center" }}>
-            <span style={{cursor:"pointer",padding:"5px"}} onClick={()=>handleLeftPointer()}>⬅️</span>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            {leftPointer&&<span style={{ cursor: "pointer", padding: "5px" }} onClick={() => handleLeftPointer()} className="left__pointer">⬅️</span>}
             {
               page.map((pg) => {
                 return (
-                    <span key={pg} style={{padding:"5px",border:"1px solid black", fontSize:15,cursor:"pointer"}} onClick={()=>handlePointer(pg)}>{pg}</span>
+                  <span key={pg} style={{ padding: "5px", border: "1px solid black", fontSize: 15, cursor: "pointer" }} onClick={() => handlePointer(pg)}>{pg}</span>
                 )
               })
             }
-            <span style={{cursor:"pointer",padding:"5px"}} onClick={()=>handleRightPointer()}>➡️</span>
+            {rightPointer&&<span style={{ cursor: "pointer", padding: "5px" }} onClick={() => handleRightPointer()} className="right__pointer">➡️</span>}
           </div>
         </>) : (<h1>Loading....</h1>)}
     </>
