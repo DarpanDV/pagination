6c635329e9e9d280d7c9e49be0a1dbfb04a43e9b
DarpanDV <varshdarpan@gmail.com>
2025-01-05 11:59:19 +0530

    PAgination project with full functionality

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react"
+import "../src/App.css";
+function App() {
+
+  const [product, setProduct] = useState([])
+  const [page, setPage] = useState([]);
+  const[pagePointer,setPagePointer]=useState(1);
+  useEffect(() => {
+    fetchApi()
+  }, [])
+
+  useEffect(()=>{
+    if(product.length!=0)
+    {
+      setPage(Array.from({ length: product.length / 10 }, (_, id) => id + 1));
+    }
+  },[product])
+
+  const fetchApi = async () => {
+    try {
+      const res = await fetch("https://dummyjson.com/products?limit=100", {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json'
+        }
+      });
+      const data = await res.json();
+      setProduct(data.products);
+    } catch (error) {
+      console.log("error---" + error)
+    }
+
+  }
+
+  const handlePointer=(pg)=>{
+    setPagePointer(pg);
+  }
+
+  const handleLeftPointer=()=>{
+    if(pagePointer!=1)
+    setPagePointer(pagePointer=>pagePointer-1)
+  }
+
+  const handleRightPointer=()=>{
+    if(pagePointer!=product.length)
+    setPagePointer(pagePointer=>pagePointer+1)
+  }
+
+  return (
+    <>
+      {product.length != 0 ?
+        (<>
+          <div className="container">
+            {product.slice((pagePointer*10)-10,pagePointer*(product.length/10)).map((item) => {
+              return (
+                <div key={item.id}>
+                 
+                  <img src={item.thumbnail} alt={item.title} />
+                  <p>{item.title}</p>
+                </div>)
+            })
+            }
+          </div>
+          <div style={{ display: "flex",justifyContent:"center" }}>
+            <span style={{cursor:"pointer",padding:"5px"}} onClick={()=>handleLeftPointer()}>⬅️</span>
+            {
+              page.map((pg) => {
+                return (
+                    <span key={pg} style={{padding:"5px",border:"1px solid black", fontSize:15,cursor:"pointer"}} onClick={()=>handlePointer(pg)}>{pg}</span>
+                )
+              })
+            }
+            <span style={{cursor:"pointer",padding:"5px"}} onClick={()=>handleRightPointer()}>➡️</span>
+          </div>
+        </>) : (<h1>Loading....</h1>)}
+    </>
+  )
+}
+
+export default App
